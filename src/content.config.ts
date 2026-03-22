2211977ea74aaf8eccfdbb4adbf5d24cae2571b8
Dominion Gbadamosi <worshipgbadamosi@live.com>
2026-03-22 11:07:20 +0000

    blog: list view added

--- src/content.config.ts
+++ src/content.config.ts
@@ -9,6 +9,7 @@ const blog = defineCollection({
     pubDate: z.coerce.date(),
     author: z.string().optional(),
     tags: z.array(z.string()).optional(),
+    heroImage: z.string().optional(),
   }),
 });
 
